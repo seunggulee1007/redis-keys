3f489f540e40df9de46b39a81906a5a167450d55
이승구 <leesg107@naver.com>
2024-06-13 17:28:06 +0900

    May 13 11:23:51 2024 KST
    feat: first commit

---
+++ src/test/java/com/yeseung/rediskeys/RedisKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeseung.rediskeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisKeysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
